c3406ca6cb98b1fe7c254af492f0dfc721a21782
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-03-05 11:35:35 +0000

    Added tests for unuccessful login to ensure auth token is not set

--- test/services/authentication.test.dart
+++ test/services/authentication.test.dart
@@ -8,9 +8,15 @@ void main() async {
     DioAdapter dioAdapter;
 
     setUpAll(() {
+      // Set up mock dio adapter before all tests
       dioAdapter = DioAdapter();
 
       Http().dio.httpClientAdapter = dioAdapter;
+    });
+
+    setUp(() {
+      // Reset auth token before each test
+      Http().aToken = '';
     });
 
     group('logging in', () {
@@ -22,6 +28,18 @@ void main() async {
         await AuthenticationService().login('Test email', 'test password');
 
         expect(Http().aToken, 'Token: 12345');
+      });
+
+      test('unsuccessfully', () async {
+        dioAdapter
+            .onPost('/users/login')
+            .reply(422, {}, headers: {'set-cookie': []});
+
+        // Confirm auth token is not set
+        expect(AuthenticationService().login('Test email', 'test password'),
+            throwsException);
+
+        expect(Http().aToken, '');
       });
     });
   });
