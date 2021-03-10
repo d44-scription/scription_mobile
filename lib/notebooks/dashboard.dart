c814071485247f096de33bf4a3fa965413309ff6
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-03-10 15:07:57 +0000

    Added framework for links to notable pages

--- lib/notebooks/dashboard.dart
+++ lib/notebooks/dashboard.dart
@@ -17,8 +17,30 @@ class _DashboardState extends State<Dashboard> {
     return Scaffold(
         appBar: AppBar(title: Text(widget.notebook.name)),
         body: Center(
-            child: Column(
-          children: [Text(widget.notebook.summary ?? '', style: Styles.body())],
-        )));
+            child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text(widget.notebook.summary ?? '', style: Styles.body()),
+                    ListTile(
+                        leading: Icon(Icons.person, size: 48),
+                        title: Text("Characters"),
+                        onTap: () {},
+                        subtitle: Text(
+                            "View characters for ${widget.notebook.name}")),
+                    ListTile(
+                        leading: Icon(Icons.home, size: 48),
+                        title: Text("Locations"),
+                        onTap: () {},
+                        subtitle:
+                            Text("View locations for ${widget.notebook.name}")),
+                    ListTile(
+                        leading: Icon(Icons.hardware, size: 48),
+                        title: Text("Items"),
+                        onTap: () {},
+                        subtitle:
+                            Text("View items for ${widget.notebook.name}")),
+                  ],
+                ))));
   }
 }
