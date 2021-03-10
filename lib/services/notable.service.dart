9fc924b0485dce5d3d40aac7497ee651cb1f6c5b
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-03-10 15:55:34 +0000

    Added notable service

---
+++ lib/services/notable.service.dart
@@ -0,0 +1,11 @@
+import 'package:scription_mobile/http-common.dart';
+
+class NotableService {
+  // GET notables index
+  Future index(int notebookId, String type) async {
+    final response =
+        await Http().dio.get("/notebooks/$notebookId/${type.toLowerCase()}");
+
+    return response.data;
+  }
+}
