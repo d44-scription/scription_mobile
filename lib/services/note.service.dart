314801df7862822634db17673674c9cd6d4c792e
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-03-12 15:27:03 +0000

    Formatting fixes

--- lib/services/note.service.dart
+++ lib/services/note.service.dart
@@ -3,7 +3,9 @@ import 'package:scription_mobile/http-common.dart';
 class NoteService {
   // GET notable notes
   Future index(notebookId, notableId) async {
-    final response = await Http().dio.get('/notebooks/$notebookId/notables/$notableId/notes');
+    final response = await Http()
+        .dio
+        .get('/notebooks/$notebookId/notables/$notableId/notes');
 
     return response.data;
   }
