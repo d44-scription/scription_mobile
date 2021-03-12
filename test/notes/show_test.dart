cb74b074993783a4ca1c721003f377a5a71ebfc5
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-03-12 15:17:58 +0000

    Added test to confirm full note is shown

---
+++ test/notes/show_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:scription_mobile/models/note.dart';
+import 'package:scription_mobile/notes/show.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  group('Notes show widget', () {
+    final note1 = Note(
+        id: 1,
+        content:
+            'Note has content for @[Character 1](@1), #[Location 2](#2), and :[Item 3](:3)');
+    final note2 = Note(id: 2, content: 'X' * 151);
+
+    testWidgets('Rendering regex-cleaned text', (WidgetTester tester) async {
+      final widget = new MediaQuery(
+          data: new MediaQueryData(),
+          child: new MaterialApp(home: Show(note: note1)));
+
+      await tester.pumpWidget(widget);
+
+      // Confirm content is shown with mentions removed
+      expect(
+          find.text('Note has content for Character 1, Location 2, and Item 3'),
+          findsOneWidget);
+    });
+
+    testWidgets('Rendering full text', (WidgetTester tester) async {
+      final widget = new MediaQuery(
+          data: new MediaQueryData(),
+          child: new MaterialApp(home: Show(note: note2)));
+
+      await tester.pumpWidget(widget);
+
+      // Confirm content is show untruncated
+      expect(find.text('X' * 151), findsOneWidget);
+    });
+  });
+}
