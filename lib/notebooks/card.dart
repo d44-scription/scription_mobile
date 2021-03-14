5743fa494f0d4eae1366101d4059d7fb863f7cfb
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-03-14 16:51:26 +0000

    Converted notebooks index to use standard list page

--- lib/notebooks/card.dart
+++ lib/notebooks/card.dart
@@ -4,9 +4,9 @@ import 'package:scription_mobile/notebooks/dashboard.dart';
 import 'package:scription_mobile/styles.dart';
 
 class NotebookCard extends StatefulWidget {
-  NotebookCard({Key key, this.notebook}) : super(key: key);
+  NotebookCard({Key key, this.data}) : super(key: key);
 
-  final Notebook notebook;
+  final Map<String, dynamic> data;
 
   @override
   _NotebookCardState createState() => _NotebookCardState();
@@ -15,14 +15,16 @@ class NotebookCard extends StatefulWidget {
 class _NotebookCardState extends State<NotebookCard> {
   @override
   Widget build(BuildContext context) {
+    Notebook _notebook = Notebook.fromJson(widget.data);
+
     return ListTile(
-      title: Text(widget.notebook.name, style: Styles.title()),
-      subtitle: Text(widget.notebook.summary ?? '', style: Styles.body()),
+      title: Text(_notebook.name, style: Styles.title()),
+      subtitle: Text(_notebook.summary ?? '', style: Styles.body()),
       onTap: () {
         Navigator.push(
             context,
             MaterialPageRoute(
-                builder: (context) => Dashboard(notebook: widget.notebook)));
+                builder: (context) => Dashboard(notebook: _notebook)));
       },
     );
   }
--- lib/notebooks/index.dart
+++ lib/notebooks/index.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scription_mobile/notebooks/card.dart';
-import 'package:scription_mobile/models/notebook.dart';
 import 'package:scription_mobile/services/notebook.service.dart';
-import 'package:scription_mobile/styles.dart';
-import 'package:scription_mobile/constants.dart' as Constants;
+import 'package:scription_mobile/index.dart';
 
 class Notebooks extends StatefulWidget {
   @override
@@ -11,64 +9,16 @@ class Notebooks extends StatefulWidget {
 }
 
 class _NotebooksState extends State<Notebooks> {
-  List<Notebook> _notebooks = [];
-  bool _loading = true;
-
-  Future<void> _getNotebooks() async {
-    setState(() {
-      _loading = true;
-    });
-
-    NotebookService().index().then((response) {
-      List<Notebook> newList = [];
-
-      for (Map<String, dynamic> data in response) {
-        newList.add(Notebook.fromJson(data));
-      }
-
-      setState(() {
-        _notebooks = newList;
-        _loading = false;
-      });
-    });
-  }
-
-  Widget _renderList() {
-    if (_loading) {
-      return CircularProgressIndicator();
-    } else if (!_loading && _notebooks.isEmpty) {
-      return Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(children: [
-            Text('No notebooks added!', style: Styles.title()),
-            Divider(),
-            Text(Constants.VISIT_WEB_APP, style: Styles.body())
-          ]));
-    } else {
-      return ListView.separated(
-        padding: const EdgeInsets.all(8),
-        itemCount: _notebooks.length,
-        itemBuilder: (BuildContext context, int index) =>
-            NotebookCard(notebook: _notebooks[index]),
-        separatorBuilder: (BuildContext context, int index) => const Divider(),
-      );
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _getNotebooks();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar:
             AppBar(title: Text('Notebooks'), automaticallyImplyLeading: false),
-        body: RefreshIndicator(
-          child: Center(child: _renderList()),
-          onRefresh: _getNotebooks,
-        ));
+        body: Index(
+            display: ({Map<String, dynamic> data}) {
+              assert(data != null);
+              return NotebookCard(data: data);
+            },
+            callback: NotebookService().index));
   }
 }
