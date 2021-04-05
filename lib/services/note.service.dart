d21fc3f58028d87ede34e1372f56d8a849779332
ctrlaltdelete44 <anthony.davies@plymouthsoftware.com>
2021-04-05 15:28:21 +0100

    Added support for retrieving unlinked notes

--- lib/services/note.service.dart
+++ lib/services/note.service.dart
@@ -3,9 +3,11 @@ import 'package:scription_mobile/http-common.dart';
 class NoteService {
   // GET notable notes
   Future index(notebookId, notableId) async {
-    final response = await Http()
-        .dio
-        .get('/notebooks/$notebookId/notables/$notableId/notes');
+    final response = notableId == null
+        ? await Http().dio.get('/notebooks/$notebookId/notes/unlinked')
+        : await Http()
+            .dio
+            .get('/notebooks/$notebookId/notables/$notableId/notes');
 
     return response.data;
   }
